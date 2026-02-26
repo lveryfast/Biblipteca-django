7d12c57549b230ffe993d1cf86e12d46a678703d
lveryfast <danyprorodriguez12@hotmail.com>
2026-02-26 17:16:06 -0500

    URLs, prueba de server y migraciones

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def book_list(request):
+    # Vista temporal para probar que el servidor corre
+    return render(request, 'catalog/books.html')
